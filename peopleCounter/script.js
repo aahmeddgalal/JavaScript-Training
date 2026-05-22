7dd7e8f2d0d85bc59a63e5ee40976ff57d509257
Ledger <dr-jermy@outlook.com>
2026-05-22 16:36:00 +0300

    reset- save functions

--- peopleCounter/script.js
+++ peopleCounter/script.js
@@ -15,3 +15,15 @@ function decrement() {
     }
     document.getElementById("counter").innerText = peopleCount;
 }
+
+function save() {
+    let results = document.getElementById("results");
+    results.textContent += peopleCount + " - ";
+}
+
+function reset() {
+    peopleCount = 0;
+    document.getElementById("counter").innerText = peopleCount;
+    document.getElementById("results").textContent = "latest: ";
+}
+
