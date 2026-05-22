dd0dc017a86deeebf1726baa63e2b647b6fb87d9
Ledger <dr-jermy@outlook.com>
2026-05-22 16:43:42 +0300

    sorry forgot to test :) here's the improved version

--- peopleCounter/script.js
+++ peopleCounter/script.js
@@ -16,14 +16,18 @@ function decrement() {
     document.getElementById("counter").innerText = peopleCount;
 }
 
-function save() {
-    let results = document.getElementById("results");
-    results.textContent += peopleCount + " - ";
-}
-
 function reset() {
     peopleCount = 0;
     document.getElementById("counter").innerText = peopleCount;
     document.getElementById("results").textContent = "latest: ";
 }
+
+function save() {
+    let results = document.getElementById("results");
+    results.textContent += peopleCount + " - ";
+    peopleCount = 0;
+    document.getElementById("counter").innerText = peopleCount;
+}
+
+
 
