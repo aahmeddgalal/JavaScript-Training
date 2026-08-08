2a89d346dd7b83eeb4f2c448697c1ed90101ef51
Galileo <aahmeddgalal@gmail.com>
2026-08-08 20:55:33 +0300

    fibonacci odd Sum

---
+++ oddFibonacciSum.js
@@ -0,0 +1,17 @@
+function sumFibs(num) {
+  const fibs = [0, 1]
+  while (
+    fibs[fibs.length - 1] + fibs[fibs.length - 2] <= num) {
+    fibs.push(fibs[fibs.length - 1] + fibs[fibs.length - 2])
+  }
+  let oddFibs = 0
+  for (let i = 0; i < fibs.length; i++) {
+    if (fibs[i] % 2 !== 0) {
+      oddFibs += fibs[i]
+    }
+  }
+  console.log(fibs)
+  return oddFibs
+}
+
+console.log(sumFibs(100)) 
